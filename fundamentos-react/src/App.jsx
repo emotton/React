19a28bafb28863532ded93fdb3029e3aab359956
emotton <emotton@gmail.com>
2021-10-30 18:32:44 -0300

    Conclusão Fundamentos

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -14,6 +14,7 @@ import DiretaPai from './components/comunicacao/DiretaPai';
 import IndiretaPai from './components/comunicacao/IndiretaPai';
 import Input from './components/formulario/Input';
 import Contador from './components/contador/Contador';
+import Mega from './components/mega/Mega';
 import './App.css';
 
 /*eslint-disable */
@@ -66,6 +67,9 @@ export default () =>
             </Card>
             <Card titulo="Contador" color="#424242">
                 <Contador numeroInicial={10}></Contador>
+            </Card>
+            <Card titulo="Mega" color="#B9006E">
+                <Mega qtde={6}></Mega>
             </Card>
         </div>
     </div>;
